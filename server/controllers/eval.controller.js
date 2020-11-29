cc53325da6ab8497c7411a8aedddfdda5ff25017
eppen <diffunity@gmail.com>
2020-11-30 02:11:03 +0900

    implementation of endpoints /eval/*; pending fixes

--- server/controllers/eval.controller.js
+++ server/controllers/eval.controller.js
@@ -1,57 +1,82 @@
 const db = require('../models');
+const { parsing_data, evaluate, user } = db;
 
-const { parsing_data, evaluate } = db;
+parsing_data.hasMany(evaluate, {foreignKey: 'Pid'})
+evaluate.belongsTo(parsing_data, {foreignKey: 'Pid'})
 
-// exports.evalContent = (req, res) => {
-//   console.log(`Eval user ${req.username} sent a request`);
-
-//   evaluate.findAll({
-//     where : {
-//       Eid: req.body.Uid
-//     },
-//   }).then((evaluate) => {
-//     if(!evaluate){
-//       return res.status(404).json({
-//         message: 'evaluator does not have any data assigned'
-//       })
-//     } else {
-//       parsing_data.findAll({
-//         where: {
-//           Pid: evaluate.Pid
-//         }
-//       }).then((parsing_data) => {
-//         return res.status(200).json({
-//           "TaskName"
-//         })
-//       })
-//     }
-//   })
-//   return res.status(200).send('Eval Content.');
-// };
-
-
-exports.example = (req, res) => {
-  console.log(`LOOOOOOK HERE ${req.body}`);
-  // console.log(`LLLOOOOOOK HERE ${req.body.keys()}`)
-  evaluate.findAll({
+exports.evaluate = (req, res, next) => {
+  /* insert evaluated value */
+  // ! add timestamp
+  // ! final score metric
+  evaluate.update({
+    Score: req.body.Score,
+    Pass: req.body.Pass,
     where : {
-      Eid: 5
-    },
-  }).then((evaluate) => {
-    if(!evaluate){
-      return res.status(404).json({
-        message: 'evaluator does not have any data assigned'
-      })
-    } else {
-      parsing_data.findAll({
-        where: {
-          Pid: evaluate.Pid
-        }
-      }).then((parsing_data) => {
-        return res.status(200).json({
-          "returned":parsing_data.Pid
-        })
-      })
+      Eid: req.body.Eid,
+      Pid: req.body.Pid
     }
+  }).then((evaluate)=>{
+    parsing_data.findAll({
+      include: [{
+        model: evaluate,
+        required: true
+      }]
+    }).then((parsing_data) => {
+      if (parsing_data){
+        parsing_data.forEach( (data) => {
+          /* implement the final score metric*/
+
+        })
+      } else {
+        return res.status(404).json({
+          "message": "No such data"
+        })
+      }
+    })
   })
 }
+
+exports.saveToTaskTable = async function (req, res, next) {
+  // ! standards for adding to the tasktable
+}
+
+exports.evalContent = (req, res) => {
+  console.log(`Eval user ${req.query.username} sent a request`);
+  var assignedData = []
+  parsing_data.hasMany(evaluate, {foreignKey: 'Pid'})
+  evaluate.belongsTo(parsing_data, {foreignKey: 'Pid'})
+  user.findOne({
+    where : {
+      ID: req.query.username
+    }
+  }).then((user) => {
+    if (user) {
+      parsing_data.findAll({
+        include: [{
+          model: evaluate,
+          required: true
+        }]
+      }).then((parsing_data) => {
+        if (parsing_data){
+          parsing_data.forEach( (data) => {
+            if(data.Eid = user.Uid){
+              assignedData.push(parsing_data)
+            }
+          })
+          return res.status(200).json({
+            "assignedData": parsing_data
+          })
+        } else {
+          return res.status(404).json({
+              message: 'evaluator does not have any data assigned'
+            })
+        }
+      })
+    } else {
+      return res.status(404).json({
+        message: 'there is no evaluator yet'
+      })
+    }
+    
+  })
+};
--- server/routes/eval.js
+++ server/routes/eval.js
@@ -6,15 +6,17 @@ const router = express.Router();
 
 /* route begins with "./api/user/eval" */
 
-// router.get(
-//   '/data-list',
-//   [authJwt.verifyToken, authJwt.isEval],
-//   evalController.evalContent
-// );
+router.post(
+  '/evaluate',
+  // [authJwt.verifyToken, authJwt.isEval],
+  evalController.evaluate,
+  evalController.saveToTaskTable
+)
 
 router.get(
-  '/',
-  evalController.example
+  '/data-list',
+  // [authJwt.verifyToken, authJwt.isEval],
+  evalController.evalContent
 );
 
 module.exports = router;
