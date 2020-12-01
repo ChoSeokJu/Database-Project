b46721603aea3e1854965e590f9deee103652849
Cho, Seokju <hamacojr@yonsei.ac.kr>
2020-12-01 05:39:13 +0000

    minor changes

--- client/src/components/eval/DataEvalDialog.js
+++ client/src/components/eval/DataEvalDialog.js
@@ -8,7 +8,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import Grid from '@material-ui/core/Grid';
 import {
-  Container, TextField, MenuItem, DialogTitle, Divider,
+  Container,
+  TextField,
+  MenuItem,
+  DialogTitle,
+  Divider,
+  IconButton,
+  Tooltip,
 } from '@material-ui/core';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import Rating from '@material-ui/lab/Rating';
@@ -36,9 +42,6 @@ const useStyle = makeStyles((theme) => ({
     right: theme.spacing(1),
     top: theme.spacing(1),
   },
-  divider: {
-    marginTop: theme.spacing(2),
-  },
   finalSubmitText: {
     marginTop: 5,
     marginLeft: theme.spacing(1),
@@ -48,11 +51,7 @@ const useStyle = makeStyles((theme) => ({
   },
 }));
 
-export default function AppendOGDataTypeDialog({
-  open,
-  handleClose,
-  Pid,
-}) {
+export default function AppendOGDataTypeDialog({ open, handleClose, Pid }) {
   const dispatch = useDispatch();
   const classes = useStyle();
   const [score, setScore] = useState(10);
@@ -98,6 +97,39 @@ export default function AppendOGDataTypeDialog({
       <DialogContent>
         <Container>
           <Grid container>
+            <Grid item xs={12}>
+              <Typography variant="h6">Pass/Non-Pass</Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Tooltip title="Pass" placement="right">
+                <IconButton onClick={() => setPNP(true)}>
+                  <CheckIcon
+                    style={{
+                      fontSize: 150,
+                      color: PNP ? green[500] : grey[500],
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Tooltip title="Non-Pass" placement="right">
+                <IconButton onClick={() => setPNP(false)}>
+                  <CloseIcon
+                    style={{
+                      fontSize: 150,
+                      color: PNP ? grey[500] : red[500],
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+
             <Grid item xs={12} className={classes.opinion}>
               <TextField
                 variant="outlined"
@@ -106,58 +138,53 @@ export default function AppendOGDataTypeDialog({
                 fullWidth
                 onChange={handleOpinionChange}
                 value={opinion}
-                placeholder="의견을 적어주세요"
+                placeholder="상세 의견을 적어주세요"
                 multiline
                 rows={10}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box>
-                <Card style={{ width: 'fit-content' }} onClick={() => setPNP(true)}>
-                  <CardActionArea>
-                    <CheckIcon style={{ fontSize: 150, color: PNP ? green[500] : grey[500] }} />
-                    <Box display="flex" justifyContent="center" m={1}>
-                      <Typography>
-                        Pass
-                      </Typography>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box display="flex" justifyContent="flex-end">
-                <Card style={{ width: 'fit-content' }} onClick={() => setPNP(false)}>
-                  <CardActionArea>
-                    <CloseIcon style={{ fontSize: 150, color: PNP ? grey[500] : red[500] }} />
-                    <Box display="flex" justifyContent="center" m={1}>
-                      <Typography>
-                        Non-Pass
-                      </Typography>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Box>
-            </Grid>
+
             <Grid item xs={12}>
-              <Divider className={classes.divider} />
+              <Divider />
             </Grid>
+
             <Grid item xs={12}>
-              <Box component="fieldset" borderColor="transparent" display="flex" justifyContent="space-between">
+              <Box
+                component="fieldset"
+                borderColor="transparent"
+                display="flex"
+                justifyContent="space-between"
+              >
                 <Typography className={classes.finalSubmitText}>
                   최종 점수
                 </Typography>
-                <Rating name="pristine" value={score} onChange={handleScoreChange} precision={0.5} size="large" />
+                <Rating
+                  name="pristine"
+                  value={score}
+                  onChange={handleScoreChange}
+                  precision={0.5}
+                  size="large"
+                />
               </Box>
             </Grid>
           </Grid>
         </Container>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary" align="right" Icon={<CheckIcon />}>
+        <Button
+          onClick={handleClose}
+          color="primary"
+          align="right"
+          Icon={<CheckIcon />}
+        >
           취소
         </Button>
-        <Button onClick={handleSubmit} color="primary" variant="contained" Icon={<CloseIcon />}>
+        <Button
+          onClick={handleSubmit}
+          color="primary"
+          variant="contained"
+          Icon={<CloseIcon />}
+        >
           평가 완료
         </Button>
       </DialogActions>
