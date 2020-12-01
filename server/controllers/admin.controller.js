252408112090a4fba43f096ed05b5bab80cb8876
YooGunWook <gunwook0307@yonsei.ac.kr>
2020-12-01 21:19:38 +0900

    fix: code

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,8 +1,11 @@
 const db = require('../models');
 const config = require('../config/auth.config');
+const { json } = require('body-parser');
+const { response } = require('express');
 
 const User = db.user;
 const Task = db.task;
+const ogData = db.og_data_type
 const { Op } = db.Sequelize;
 const works_on = db.works_on
 
@@ -24,43 +27,42 @@ exports.getTask = (req, res) => {
     }))
 };
 
-exports.applyTask = (req, res) => {
-  const {taskName, Uid} = req.body
-  works_on.create({
-    'Sid': Uid,
-    'TaskName': taskName,
-    'permit': 0,
-  }).then((user) => {
-    res.json({
-      message: '성공적으로 신청되었습니다.'
-    })
-  })
-};
 
 exports.approveUser = (req, res) => {
   const {taskName, Uid} = req.body
-  works_on.findByPk(Uid).then((user) => {
-    user.set('permit', 1)
-    user.save()
-    return res.status(200).json({
-      message: '해당 Task를 승인했습니다'
-    })
-  })
+  works_on.findOne({where: {TaskName: taskName, Sid: Uid}}).then((result) =>{
+    if (result.get('Permit') === 0) {
+      result.set('Permit', 1)
+      result.save()
+      return res.status(200).json({
+      message: '해당 Task의 참여를 승인했습니다'
+      })
+    }
+    else {
+      return res.status(400).json({
+        message: '해당 유저는 이미 승인 결과가 나왔습니다'
+      })
+    }
+})
 };
+  
 
 exports.rejectUser = (req, res) => {
   const {taskName, Uid} = req.body
-  works_on.findByPk(Uid).then((user) => {
-    user.set('permit', 2)
-    user.save()
-    return res.status(200).json({
-      message: '해당 Task를 거절함'
-    })
-  })
-};
-
-exports.rejectUser = (req, res) => {
-  const {taskName, Uid} = req.body
+  works_on.findOne({where: {TaskName: taskName, Sid: Uid}}).then((result) =>{
+    if (result.get('Permit') === 0) {
+      result.set('Permit', null)
+      result.save()
+      return res.status(200).json({
+      message: '해당 Task의 참여를 거절했습니다'
+      })
+    }
+    else {
+      return res.status(400).json({
+        message: '해당 유저는 이미 승인 결과가 나왔습니다'
+      })
+    }
+})
 };
 
 exports.pendingUser = (req, res) => {
@@ -76,12 +78,13 @@ exports.pendingUser = (req, res) => {
         attributes: ['Uid', 'ID', 'Name'],
         required: true}
       ],
-      where: {TaskName: taskName, permit:0},
+      where: {TaskName: taskName, Permit:0},
       offset: parseInt(per_page)*(parseInt(page)-1),
       limit: parseInt(per_page)}).then((result) => {
         for (var i=0; i<result.length; i++) {
           arr.push(result[i].user)
         }
+        console.log(JSON.stringify(result))
         res.json({
           'data': arr,
           'page': parseInt(page),
@@ -103,7 +106,7 @@ exports.approvedUser = (req, res) => {
         attributes: ['Uid', 'ID', 'Name'],
         required: true}
       ],
-      where: {TaskName: taskName, permit:1},
+      where: {TaskName: taskName, Permit:1},
       offset: parseInt(per_page)*(parseInt(page)-1),
       limit: parseInt(per_page)}).then((result) => {
         for (var i=0; i<result.length; i++) {
@@ -117,3 +120,39 @@ exports.approvedUser = (req, res) => {
       })))
 };
 
+exports.getSchema = (req, res) => {
+  const {taskName} = req.query
+  var arr = []
+  Task.findAll({
+    attributes: ['TableSchema'],
+    where: {TaskName: taskName}})
+    .then((columns) => {
+      console.log(Object.keys(columns[0].TableSchema[0])[0])
+      for (var i=0; i<columns[0].TableSchema.length; i++) {
+        arr.push({columnName: Object.keys(columns[0].TableSchema[i])[0]})
+      }
+      res.status(200).json({
+        data: arr
+      })
+    })
+};
+
+exports.addTask = (req, res) => {
+};
+
+exports.addOgData = (req, res) => {
+  const {taskName, OGDataType, data} = req.body
+  ogData.findAll()
+};
+
+
+exports.test = (req, res) => {
+  const {taskName, Uid} = req.body
+  works_on.findOne({where: {TaskName: taskName, Sid: Uid}}).then((result) =>{
+    if (result.get('Permit') === 1) {
+      result.set('Permit', 3)
+      result.save()
+      res.json(result)
+    }}
+  );
+};
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -12,9 +12,11 @@ router.get(
 
 router.get('/task', adminController.getTask);
 router.get('/task/approve', adminController.approveUser)
-router.get('task/pending', adminController.pendingUser);
-router.get('task/approved', adminController.pendingUser);
-
+router.get('/task/reject', adminController.rejectUser)
+router.get('/task/pending', adminController.pendingUser);
+router.get('/task/approved', adminController.pendingUser);
+router.get('/task/schema', adminController.getSchema);
+router.get('/task/test', adminController.test);
 
 module.exports = router;
 
