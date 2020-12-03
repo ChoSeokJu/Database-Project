eb9b073659fbc04dc00bad33bdc66af6b72907c2
YooGunWook <gunwook0307@yonsei.ac.kr>
2020-12-03 17:05:36 +0900

    fix: code account

--- server/routes/account.js
+++ server/routes/account.js
@@ -18,8 +18,4 @@ router.post('/signin', controller.signin);
 
 router.get('/example', controller.getUser);
 
-router.get('/examples', function (req, res) {
-  return res.status(200).send('시발')
-});
-
 module.exports = router;
