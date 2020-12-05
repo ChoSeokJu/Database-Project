c836c825100d9faa1129aae5fd7d64b95c725431
eppen <diffunity@gmail.com>
2020-12-05 22:39:39 +0900

    fix bug

--- server/routes/submit.js
+++ server/routes/submit.js
@@ -41,7 +41,7 @@ router.get(
 
 router.get(
   '/submitter-list',
-  // [authJwt.verifyToken, authJwt.isSubmit],
+  [authJwt.verifyToken, authJwt.isSubmit],
   submitController.getSubmitterList,
   submitController.groupSubmitterList
 )
